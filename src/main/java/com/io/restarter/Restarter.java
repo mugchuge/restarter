776af15a37e41757e24287dcd312598a107d16b8
nun <92763765+mugchuge@users.noreply.github.com>
2021-11-06 09:08:25 +0900

    안내만 무한 반복되던 버그 수정

--- src/main/java/com/io/restarter/Restarter.java
+++ src/main/java/com/io/restarter/Restarter.java
@@ -10,6 +10,7 @@ public class Restarter implements Runnable {
     public void run() {
         if (num == 60) {
             System.out.println("1분뒤 서버가 재시작됩니다.");
+            num = num - 1;
         } else if (num == 0){
             System.out.println("서버를 닫습니다.");
             Bukkit.shutdown();
